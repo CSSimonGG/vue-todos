2b7aafe6245fc16f1b7ac07781decb3563a8f417
Simon <satmdeklerk@gmail.com>
2023-06-03 15:20:54 +0200

    Styles added

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { uid } from "uid";
 import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
 
 const todoList = ref([]);
 
@@ -20,6 +21,9 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
   </main>
 </template>
 
@@ -35,6 +39,21 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
